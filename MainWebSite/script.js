f7080a855e084b3f843837d1fec5ea12ec4f669a
Markusdeatrius <ljhb@email.cz>
2025-04-30 00:11:06 +0200

    little bit done

--- MainWebSite/script.js
+++ MainWebSite/script.js
@@ -1,5 +1,6 @@
 let navbar = document.querySelector('.header .flex .navbar')
 let menuBtn = document.querySelector('#menu-btn')
+let header = document.querySelector('.header')
 
 menuBtn.onclick = () => {
     navbar.classList.toggle('active')
@@ -9,4 +10,10 @@ menuBtn.onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active')
     menuBtn.classList.remove('fa-times')
+
+    if(window.scrollY > 0){
+        header.classList.add('active')
+    }else {
+        header.classList.remove('active')
+    }
 }
