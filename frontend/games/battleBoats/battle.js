48096e880133ca4577fe88f5d8642a67a91308bc
Markusdeatrius <ljhb@email.cz>
2025-05-23 20:03:15 +0200

    LittleBitWorkDoneOnBoats

--- frontend/games/battleBoats/battle.js
+++ frontend/games/battleBoats/battle.js
@@ -40,8 +40,9 @@ createBoard('pink', 'player')
 createBoard('green', 'computer')
 //GameBoard Done
 
-//Creating Ships start
 
+
+//Creating Ships start
 class Ship {
     constructor(name, length){
         this.name = name
@@ -52,15 +53,33 @@ class Ship {
 const destroyer = new Ship("destroyer", 2)
 const submarine = new Ship("submarine", 3)
 const cruiser = new Ship("cruiser", 3)
-const battleShip = new Ship("battleShip", 4)
+const battleShip = new Ship("battleship", 4)
 const carrier = new Ship("carrier", 5)
 
 const ships = [destroyer, submarine, cruiser, battleShip, carrier]
 
-function addShipPiece() {
-    const allBoardBlocks = document.querySelector('#computer div')
+function addShipPiece(ship) {
+    const allBoardBlocks = document.querySelectorAll('#computer div')
+    let randomBoolean = Math.random() < 0.5
+    let isHorizontal = randomBoolean
     let randomStartIndex = Math.floor(Math.random() * width * width)
-    console.log(randomStartIndex)
+
+    //let validStart = isHorizontal ? randomStartIndex <= width * width - ship.length
+
+    let shipBlocks = []
+
+    for (let i = 0; i < ship.length; i++) {
+        if (isHorizontal){
+            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i])
+        }else {
+            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i * width])
+        }
+    }
+
+    shipBlocks.forEach(shipBlock => {
+        shipBlock.classList.add(ship.name)
+        shipBlock.classList.add('taken')
+    })
 }
 
-addShipPiece()
+ships.forEach(ship => addShipPiece(ship))
