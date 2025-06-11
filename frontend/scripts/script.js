56bb0dcfe8e5a5f262a4df34a0bc2b94b59db5fb
Markusdeatrius <ljhb@email.cz>
2025-06-11 16:03:36 +0200

    pages

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -16,3 +16,36 @@ window.onscroll = () => {
         header.classList.remove('active')
     }
 }
+
+// Načtení dat při načtení stránky
+document.addEventListener('DOMContentLoaded', () => {
+    fetchUsers();
+});
+
+
+
+// Přidání novevého usera
+document.getElementById('body').addEventListener('submit', async (e) => { //nemelo by byt kvuli db async
+    e.preventDefault();
+    const name = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    try {
+        const response = await fetch('http://localhost:3000/api/users', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name, password })
+        });
+        
+        if (response.ok) {
+            document.getElementById('body').reset();
+            fetchUsers();
+        } else {
+            alert('Chyba při přidávání usera');
+        }
+    } catch (error) {
+        console.error('Chyba:', error);
+    }
+});
+
+document.getElementById('body').addEventListener('')
